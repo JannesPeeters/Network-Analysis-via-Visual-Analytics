fb9307c7a656549f1f34f4258cbf252045543ec1
Jannes Peeters <47836347+JannesPeeters@users.noreply.github.com>
2020-03-26 17:46:32 +0100

    Update stad.js

--- javascript/stad.js
+++ javascript/stad.js
@@ -1,9 +1,37 @@
-// r2d3: https://rstudio.github.io/r2d3
-//
 svg.selectAll("*").remove();
 
-var colorScale = d3.scaleOrdinal().domain(['2', '3', '4', '5', '6', '7'])
-                                  .range(["grey", "brown", "blue", "darkgreen", "purple", "orange", "yellow"]);
+Shiny.addCustomMessageHandler('colorUpdate', function(message) {
+  window.colorVar = message.colorVariable;
+  //alert(JSON.stringify(window.colorVar));
+  
+  if(d3.map(data.nodes, function(d){return d[window.colorVar];} ).keys().length < 7) {
+    var colorScale = d3.scaleOrdinal().domain(d3.map(data.nodes, function(d){return d[window.colorVar];} ).keys())
+                                  .range(d3.schemeSet1);
+  } else {
+    var colorScale = d3.scaleSequential(d3.interpolateOrRd).domain([d3.min(data.nodes, function(d){return d[window.colorVar]}),d3.max(data.nodes, function(d){return d[window.colorVar]})]);
+  }
+    
+
+  node.style('fill', d => colorScale(d[window.colorVar]));
+});
+
+Shiny.addCustomMessageHandler('nodeUpdate', function(message) {
+  window.nodeSize = message.newSize;
+  
+  node.attr("r", window.nodeSize);
+});
+
+Shiny.addCustomMessageHandler('nodeByObject', function(message) {
+  window.nodeObject = message.nodeVariable;
+  
+  var nodeScale = d3.scaleLinear().domain([d3.min(data.nodes, function(d){return d[window.nodeObject]}),d3.max(data.nodes, function(d){return d[window.nodeObject]})]).range([20,90]);
+  
+  node.attr("r", d => nodeScale(d[window.nodeObject]));
+});
+
+
+//var colorScale = d3.scaleOrdinal().domain(d3.map(data.nodes, function(d){return d[window.colorVar];} ).keys())
+//                                  .range(d3.schemeSet1);
 
 var g = svg.append("g")
              .attr("class", "network-area");
@@ -20,11 +48,14 @@ var node = g.append("g").attr("class", "nodes")
                             .data(data.nodes)
                             .enter()
                             .append("circle")
-                              .attr("r", 10)
-                              .style("fill", d => colorScale(d.hh_size))
+                              .attr("r", 40)
+                              .call(d3.drag()
+                                      .on("start", dragstarted)
+                                      .on("drag", dragged)
+                                      .on("end", dragended))
                               .on('mouseover.fade', fade(0.1))
                               .on('mouseout.fade', fade(1))
-                              .attr("d", function(d) { return d.id; })
+                              .attr("d", function(d) { return d.label; })
                               .on("mouseover", function(){
                                 Shiny.setInputValue("node_clicked", 
                                                     d3.select(this).attr("d"),
@@ -42,7 +73,6 @@ var node = g.append("g").attr("class", "nodes")
                       .force("charge", d3.forceManyBody().strength(-600))
                       .force("center", d3.forceCenter(width / 2, height / 2))
                       .on("tick", ticked);
-                    
     
   function zoom_actions(){
     g.attr("transform", d3.event.transform);
@@ -85,3 +115,21 @@ const linkedByIndex = {};
 function isConnected(a, b) {
     return linkedByIndex[`${a.index},${b.index}`] || linkedByIndex[`${b.index},${a.index}`] || a.index === b.index;
   }
+
+
+function dragstarted(d) {
+  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+  d.fx = d.x;
+  d.fy = d.y;
+}
+
+function dragged(d) {
+  d.fx = d3.event.x;
+  d.fy = d3.event.y;
+}
+
+function dragended(d) {
+  if (!d3.event.active) simulation.alphaTarget(0);
+  d.fx = null;
+  d.fy = null;
+}
